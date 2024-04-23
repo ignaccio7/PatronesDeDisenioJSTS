89463ffdca1d6364d49cf44ad3c9813da47ff98b
ignaccio7 <nacho260298@gmail.com>
2024-04-23 18:23:22 -0400

    Fix workflow

--- home-page/astro.config.mjs
+++ home-page/astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ignaccio7.github.io',
-  base: '/PatronesDeDisenioJSTS/',
+  site: 'https://ignaccio7.github.io/PatronesDeDisenioJSTS',
+  base: 'PatronesDeDisenioJSTS',
   integrations: [tailwind()]
 });
